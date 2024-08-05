e774ad089cf4344ac5129e3dda7f2ab44c5d9c98
Trung Vu <vu.trung.96@gmail.com>
2024-08-05 11:33:31 -0400

    fix path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/llm-aggrefact' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/llm-aggrefact' : '',
 };
