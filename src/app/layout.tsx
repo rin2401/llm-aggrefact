69a5028f56052eec3ebc00320910cf5b7d1fbed4
Liyan06 <lytang06@gmail.com>
2024-08-05 21:07:28 -0400

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { NavBar } from "@/components/NavBar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-DMWFXSYD87"/>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DMWFXSYD87');
+          `}
+        </Script>
         <link rel="icon" href="/site-logo.svg" sizes="any" />
       </head>
       <body className="{inter.className}">
